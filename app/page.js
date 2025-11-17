a4603721ce777d565a76a127d664a484ea5487a8
kestryix <verilim3802@gmail.com>
2025-11-17 16:25:13 +0900

    initial, login and sign up pages

--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 
-export default function Home() {
+export default function InitialPage() {
   return (
     <main className={styles.main}>
       <div className={styles.header}>
-        <h1>Amuse</h1>
-        <p>Welcome to your Next.js app</p>
+        <Image
+          src="/images/logo_dark_large.png"
+          alt="Amuse"
+          width={200}
+          height={50}
+          className={styles.logo}
+          priority
+        />
+        <div className={styles.buttonContainer}>
+          <Link href="/login" className={styles.loginButton}>Log In</Link>
+          <Link href="/signup" className={styles.signUpButton}>Sign Up</Link>
+        </div>
       </div>
     </main>
   )
